18ead7acf8e130bf98f481bf3b33105707ff598a
Eduardo Cintra <educin15@hotmail.com>
2020-08-10 23:26:20 -0300

    Create the visual of main page

--- web/src/pages/Balanco.js
+++ web/src/pages/Balanco.js
@@ -1,35 +1,35 @@
 import React from 'react';
 
 const Balanco = () => {
-    // const [ descricao, setDescricao ] = useState('')
-    // const [ valor, setValor ] = useState('')
-
-    // function handleInputDesc(value) {
-    //     setDescricao(value)
-    // }
-    // function handleInputValor(value) {
-    //     setValor(value)
-    // }
 
     return (
-        <h1>Balanço</h1>
-        // <Container fluid="md">
-        //     <Form onSubmit={()=>{}}>
-        //         <Form.Group controlId="formBasicDesc">
-        //             <Form.Label className="d-block font-weight-bold text-center">Descrição Crédito/Débito</Form.Label>
-        //             <Form.Control type="text" placeholder="Insira o nome/descrição da sua renda ou débito" value={descricao} onChange={e => handleInputDesc(e.target.value)} required />
-        //         </Form.Group>
-
-        //         <Form.Group controlId="formBasicBand">
-        //             <Form.Label className="d-block font-weight-bold text-center">Valor</Form.Label>
-        //             <Form.Control type="number" value={valor} onChange={e => handleInputValor(e.target.value)} required/>
-        //         </Form.Group>
-                
-        //         <Button variant="outline-success btn-block" type="submit">
-        //             Cadastrar
-        //         </Button>
-        //     </Form>
-        // </Container>
+        <React.Fragment>
+            <div className="dashboard">
+                <div className="titleDash">
+                    <span>Balanço demonstrativo do Mês de Agosto / 2020</span>
+                </div> 
+            </div>
+            <div className="cards">
+                <div className="creditoCard">
+                    <span className="titleCard">Crédito</span>
+                    <span className="value">
+                        R$0,00
+                    </span>
+                </div>
+                <div className="debitoCard">
+                    <span className="titleCard">Débito</span>
+                    <span className="value">
+                        R$0,00
+                    </span>
+                </div>
+                <div className="saldoCard">
+                    <span className="titleCard">Saldo</span>
+                    <span className="value">
+                        R$0,00
+                    </span>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
--- web/src/pages/Header.js
+++ web/src/pages/Header.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <Navbar className="Nav" variant="dark">
-        <Navbar.Brand href="#home">Minhas Finanças</Navbar.Brand>
-          <Nav className="mx-auto">
-            <Nav.Link href="/">Balanço</Nav.Link>
-            <Nav.Link href="/cartao">Cartão</Nav.Link>
-            <Nav.Link href="/entrada_saida">Entrada/Saída</Nav.Link>
-            <Nav.Link href="/item">Item</Nav.Link>
-          </Nav>
-        </Navbar>
+      <nav className="Nav">
+        <div className="logo">
+          MF$
+        </div>
+        <div id="menus">
+          <ul>
+            <li>
+              <a href="/">Balanço</a>
+            </li>
+            <li>
+              <a href="/cartao">Cartão</a>
+            </li>
+            <li>
+              <a href="/entrada_saida">Entrada/Saída</a>
+            </li>
+            <li>
+              <a href="/item">Item</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+        // <Navbar className="Nav" variant="dark">
+        // <Navbar.Brand href="#home">Minhas Finanças</Navbar.Brand>
+        //   <Nav className="mx-auto">
+        //     <Nav.Link href="/">Balanço</Nav.Link>
+        //     <Nav.Link href="/cartao">Cartão</Nav.Link>
+        //     <Nav.Link href="/entrada_saida">Entrada/Saída</Nav.Link>
+        //     <Nav.Link href="/item">Item</Nav.Link>
+        //   </Nav>
+        // </Navbar>
     )
 }
 
